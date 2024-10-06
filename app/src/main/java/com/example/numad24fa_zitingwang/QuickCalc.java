8034fe8d13029cf81e7751a6da327e8fcbd98204
Ziting Wang <dwtintchev@outlook.com>
2024-10-06 00:53:55 -0700

    added num pad

--- app/src/main/java/com/example/numad24fa_zitingwang/QuickCalc.java
+++ app/src/main/java/com/example/numad24fa_zitingwang/QuickCalc.java
@@ -1,6 +1,7 @@
 package com.example.numad24fa_zitingwang;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,22 @@ public class QuickCalc extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_quick_calc);
+
+        Button oneBtn = findViewById(R.id.oneBtn);
+        Button twoBtn = findViewById(R.id.twoBtn);
+        Button threeBtn = findViewById(R.id.threeBtn);
+        Button fourBtn = findViewById(R.id.fourBtn);
+        Button fiveBtn = findViewById(R.id.fiveBtn);
+        Button sixBtn = findViewById(R.id.sixBtn);
+        Button sevenBtn = findViewById(R.id.sevenBtn);
+        Button eightBtn = findViewById(R.id.eightBtn);
+        Button nineBtn = findViewById(R.id.nineBtn);
+        Button zeroBtn = findViewById(R.id.zeroBtn);
+        Button plusBtn = findViewById(R.id.plusBtn);
+        Button minusBtn = findViewById(R.id.minusBtn);
+        Button deleteBtn = findViewById(R.id.deleteBtn);
+        Button equalBtn = findViewById(R.id.equalBtn);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
